9a09463e2816af38bf690c94a6fe3afb96382080
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-08 12:33:26 -0600

    configuración de admin

--- libros/admin.py
+++ libros/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Libro
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    search_fields = ['nombre']
+    ordering = ['nombre']
+
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor']
+    search_fields = ['titulo', 'autor']
+    ordering = ['titulo']
