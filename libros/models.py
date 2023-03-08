ac51417cd805d1447283d85355318e1a37425b6a
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-08 11:57:29 -0600

    definidos los modelos

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=255, unique=True)
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    portada = models.ImageField(blank=True, null=True)
+    categorias = models.ManyToManyField(Categoria, related_name='libros')
+
+    def __str__(self) -> str:
+        return self.titulo
