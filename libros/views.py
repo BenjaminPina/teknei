72a82ce606b6aa3b826c9f5e53c0701ba271a549
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-08 13:53:41 -0600

    formulario de alta de categorías

--- libros/views.py
+++ libros/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+from .models import Categoria, Libro
 
 
 def index(request):
     return render(request, 'libros/index.html')
+
+
+class CategoriaCrear(SuccessMessageMixin, CreateView):
+    model = Categoria
+    form = Categoria
+    fields = ['nombre']
+    success_message = 'Categoría registrada correctamente'
+
+    def get_success_url(self):
+        return reverse('index')
+
+
+class CategoriasLista(ListView):
+    model = Categoria
--- teknei/urls.py
+++ teknei/urls.py
@@ -9,6 +9,16 @@ from libros import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path(
+        'categorias/alta',
+        views.CategoriaCrear.as_view(template_name='libros/categoria_alta.html'),
+        name='categoria_alta',
+    ),
+    path(
+        'categorias/lista',
+        views.CategoriasLista.as_view(template_name='libros/categoria_lista.html'),
+        name='categoria_lista',
+    ),
 ]
 
 urlpatterns += static(
